8e808b8d9ee02c2cdb12915a96da6795852796d2
Sarthak Gajendragadkar <146093758+Sarthak2064@users.noreply.github.com>
2024-06-10 03:17:31 +0530

    Update Tech.jsx

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-
-import { BallCanvas } from "./canvas";
+import BallCanvas from "./canvas/Ball"; // Ensure correct import path
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+      <div className="flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+            <div className="w-28 h-28" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+            </div>
+        ))}
+      </div>
   );
 };
 
